b97d90b07224a76c9691eb45e4d9393095515f52
M. Isuru Tharanga Chrishantha Perera <isuru@apache.org>
2015-10-19 12:20:56 +0530

    Added Metrics Annotation component. Added only @Timed

---
+++ components/org.wso2.carbon.metrics.annotation/src/main/java/org/wso2/carbon/metrics/annotation/Timed.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 WSO2 Inc. (http://wso2.org)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.carbon.metrics.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Measure the time
+ */
+@Inherited
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.ANNOTATION_TYPE })
+public @interface Timed {
+    /**
+     * @return The name of the timer.
+     */
+    String name() default "";
+
+    /**
+     * @return The {@link Level} for the metric
+     */
+    Level level() default Level.INFO;
+
+    /**
+     * @return If {@code true}, use the given name as an absolute name. If {@code false}, use the given name
+     *         relative to the annotated class. When annotating a class, this must be {@code false}.
+     */
+    boolean absolute() default false;
+}
