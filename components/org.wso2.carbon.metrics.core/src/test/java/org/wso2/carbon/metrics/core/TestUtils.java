3d6501a584f591ba54849e1f1f938e53fdd70155
M. Isuru Tharanga Chrishantha Perera <chrishantha@gmail.com>
2017-07-10 18:00:57 +0530

    Use mockito for mocking and removed snakeyaml dependency

--- components/org.wso2.carbon.metrics.core/src/test/java/org/wso2/carbon/metrics/core/TestUtils.java
+++ components/org.wso2.carbon.metrics.core/src/test/java/org/wso2/carbon/metrics/core/TestUtils.java
@@ -16,7 +16,6 @@
 package org.wso2.carbon.metrics.core;
 
 
-import org.easymock.EasyMock;
 import org.wso2.carbon.config.ConfigurationException;
 import org.wso2.carbon.config.provider.ConfigProvider;
 import org.wso2.carbon.config.provider.ConfigProviderImpl;
@@ -26,10 +25,14 @@ import org.wso2.carbon.secvault.SecureVault;
 import org.wso2.carbon.secvault.exception.SecureVaultException;
 
 import java.io.File;
-import java.lang.reflect.Field;
 import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 /**
  * Test Utilities.
@@ -37,26 +40,17 @@ import java.nio.file.Paths;
 public class TestUtils {
 
     public static ConfigProvider getConfigProvider(String file) throws ConfigurationException {
-        SecureVault secureVault = EasyMock.mock(SecureVault.class);
+        SecureVault secureVault = mock(SecureVault.class);
         try {
-            EasyMock.expect(secureVault.resolve(EasyMock.anyString())).andReturn("n3wP4s5w0r4"
-                    .toCharArray()).anyTimes();
+            when(secureVault.resolve(anyString())).thenReturn("n3wP4s5w0r4".toCharArray());
         } catch (SecureVaultException e) {
             throw new ConfigurationException("Error resolving secure vault", e);
-        }
-        EasyMock.replay(secureVault);
-        // Clear deploymentConfigs
-        try {
-            Field field = ConfigProviderImpl.class.getDeclaredField("deploymentConfigs");
-            field.setAccessible(true);
-        } catch (NoSuchFieldException e) {
-            // Ignore
         }
         Path carbonHome = Paths.get("");
         carbonHome = Paths.get(carbonHome.toString(), "src", "test");
         System.setProperty("carbon.home", carbonHome.toString());
-        String filePath = carbonHome.toAbsolutePath() + File.separator + "resources" + File.separator + "conf" + File
-                .separator + file;
+        String filePath = carbonHome.toAbsolutePath() + File.separator + "resources" + File.separator + "conf" +
+                File.separator + file;
         Path configurationFilePath = Paths.get(URI.create("file:" + filePath));
         ConfigFileReader configFileReader = new YAMLBasedConfigFileReader(configurationFilePath);
         return new ConfigProviderImpl(configFileReader, secureVault);
